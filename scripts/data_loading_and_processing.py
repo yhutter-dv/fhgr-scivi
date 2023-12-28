149d60413ae7ddf9c6dc3cf69329b4bfcdef79a9
yhutter <hutteryannick@gmail.com>
2023-12-28 10:23:08 +0100

    Fix generating of video overlays when having multiple poses per frame.

--- scripts/data_loading_and_processing.py
+++ scripts/data_loading_and_processing.py
@@ -96,10 +96,17 @@ def create_matplotlib_gif(tracking_data, video_information, file_path):
 def create_df_from_tracking_data(tracking_data):
 	df = []
 	for frame in tracking_data.frameData:
-	    for pose in frame.poseData:
-	    	for keypoint in pose.keyPoints:
-	    		# add frame and keypoint information to array
-	            df.append([frame.index, pose.id, keypoint.type, keypoint.posX, keypoint.posY, keypoint.absPosX, keypoint.absPosY, keypoint.absPosZ])
+		# For some reason we can get two elements in poseData which messes up the structure of the dataframe.
+		# TODO: Check why we get two poses (e.g frame 21 in file ./data/test/track_data/guemah_forehand_flat_02out.pb)
+		if len(frame.poseData) > 1:
+			print(f"Got multiple pose data for frame {frame.index}")
+		for pose in frame.poseData:
+			# We are only interested in poses with id 0, skip the rest. This only occurs when we have multiple pose data per frame.
+			if pose.id != 0:
+				continue
+			for keypoint in pose.keyPoints:
+				# add frame and keypoint information to array
+				df.append([frame.index, pose.id, keypoint.type, keypoint.posX, keypoint.posY, keypoint.absPosX, keypoint.absPosY, keypoint.absPosZ])
 
 	df = pd.DataFrame(df, columns=["frame", "pose", "keypoint", "x", "y", "absX", "absY", "absZ"])
 	df = df.pivot(index=["frame", "pose"], columns="keypoint", values=["x", "y", "absX", "absY", "absZ"])
@@ -139,40 +146,38 @@ def create_video_overlay(tracking_data, video_information, video_file_path, over
 	out = cv2.VideoWriter(overlay_file_path, cv2.VideoWriter_fourcc(*'MP4V'), fps, (res_x,res_y))
 
 	frame_number = 0
-	with console.status(f"[bold green]Creating video overlay {overlay_file_path} [/bold green]") as status:
-		while cap.isOpened():
-			success, frame = cap.read()
-			if success:
-				if frame_number in df.index.get_level_values(0):
-					x = df.loc[frame_number, pd.IndexSlice[:,['x']]].values.squeeze()
-					y = df.loc[frame_number, pd.IndexSlice[:,['y']]].values.squeeze()
+	try:
+		with console.status(f"[bold green]Creating video overlay {overlay_file_path} [/bold green]") as status:
+			while cap.isOpened():
+				success, frame = cap.read()
+				if success:
+					if frame_number in df.index.get_level_values(0):
+						x = df.loc[frame_number, pd.IndexSlice[:,['x']]].values.squeeze()
+						y = df.loc[frame_number, pd.IndexSlice[:,['y']]].values.squeeze()
 
-					for i, kp in enumerate(zip(x, y)):
-						color_tuple = hex_to_rgb(KEYPOINT_COLORS[i])
-						frame = cv2.circle(frame, (int(kp[0]), int(kp[1])), 5, color_tuple, -1)
+						for i, kp in enumerate(zip(x, y)):
+							color_tuple = hex_to_rgb(KEYPOINT_COLORS[i])
+							frame = cv2.circle(frame, (int(kp[0]), int(kp[1])), 5, color_tuple, -1)
 
-				out.write(frame)
-				frame_number += 1
-			else:
-				break
-
-	cap.release()
-	out.release()
+					out.write(frame)
+					frame_number += 1
+				else:
+					break
+	except Exception as error:
+		print(f"Failed on frame {frame_number} due to {error}")
+	finally:
+		cap.release()
+		out.release()
 
 def process(tracking_data, config):
 	video_information = get_video_information(tracking_data)
 	print(video_information)
-	try:
-		create_matplotlib_gif(tracking_data, video_information, config["matplotlib_gif_out_path"])
-	except Exception as e:
-		print(f"Failed to create matplotlib gif {config["matplotlib_gif_out_path"]}")
-		print(e)
+	create_matplotlib_gif(tracking_data, video_information, config["matplotlib_gif_out_path"])
 
 	try:
 		create_video_overlay(tracking_data, video_information, config["video_path"], config["video_overlay_out_path"])
 	except Exception as e:
-		print(f"Failed to create video overlay for video {config["video_path"]}")
-		print(e)
+		print(f"Failed to create video overlay for video {config["video_path"]} due to error {e}")
 	
 def ensure_file_path(file_path):
 	if not os.path.isfile(file_path):
